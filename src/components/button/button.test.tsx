e215e1e2d653c8c8251299c667f6dfff78e79bed
Ajit33 <justajit33@gmail.com>
2024-03-04 15:19:45 +0530

    evil commit

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -1,12 +1,17 @@
-import { describe, expect, test } from "@jest/globals";
-import { render, screen } from "@testing-library/react";
-import { Button } from "./index";
+'use client'
+interface ButtonProps{
+onClick:()=>void;
+}
 
-describe("Button Tests", () => {
-    test("should render button", () => {
-        render(<Button onClick={() => { }}>Click me</Button>);
 
-        const element = screen.getByTestId("button-test");
-        expect(element).toBeTruthy();
-    });
-});
+
+export const Button = (props: ButtonProps) => {
+    const { onClick} = props;
+    const handeler=()=>{
+        console.log("hey")
+    }
+  if(!onclick) return  <button onClick={handeler} data-testid="button-test">Button</button>
+    return (
+        <button onClick={onClick} data-testid="button-test">Button</button>
+    )
+}
