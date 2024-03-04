b0b91b99fe7324215eb40f3456952e293c1b2ad3
Ajit33 <justajit33@gmail.com>
2024-03-04 15:47:27 +0530

    purpose:- fixing test

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -7,10 +7,6 @@ onClick?:()=>void;
 
 export const Button = (props: ButtonProps) => {
     const { onClick} = props;
-    const handeler=()=>{
-        console.log("hey");
-    }
-    if(! onClick) return    <button onClick={onClick} data-testid="button-test">Button</button>
     return (
         <button onClick={onClick} data-testid="button-test">Button</button>
     )
