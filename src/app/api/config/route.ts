fa87d7047e8581931524bf7de12a808bac324800
Ajit33 <justajit33@gmail.com>
2024-03-22 01:43:15 +0530

    yuppp

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
     return Response.json(
       {
         data: {
-           navData   
+          navData,
         },
       },
       { status: 200 }
